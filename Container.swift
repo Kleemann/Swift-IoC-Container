667fe159780a75d4979e68582cc6bf7f23b51b18
Mads Kleemann <madskleemann@me.com>
2017-02-02 20:13:50 +0100

    added simple Container class

---
+++ Container.swift
@@ -0,0 +1,57 @@
+import Foundation
+
+class Container {
+    fileprivate static var registrations = [AnyHashable : () -> AnyObject]()
+    
+    
+    static func register<T>(_: T.Type, constructor: @escaping () -> AnyObject) {
+        let dependencyName = String(describing: T.self)
+        registrations[dependencyName] = constructor
+    }
+    
+    static func registerAsSingleton<T>(_: T.Type, constructor: @escaping () -> AnyObject) {
+        let dependencyName = String(describing: T.self)
+        var instance: AnyObject?
+        let resolver: () -> AnyObject = {
+            if instance == nil {
+                instance = constructor()
+                return instance!
+            } else {
+                return instance!
+            }
+        }
+        registrations[dependencyName] = resolver
+    }
+    
+    static func registerWeakSingleton<T>(_: T.Type, constructor: @escaping () -> AnyObject) {
+        let dependencyName = String(describing: T.self)
+        var instance: AnyObject?
+        instance = nil
+        let resolver: () -> AnyObject = { [weak instance] in
+            if let instance = instance {
+                return instance
+            } else {
+                let newInstance = constructor()
+                instance = newInstance
+                return newInstance
+            }
+        }
+        registrations[dependencyName] = resolver
+    }
+    
+    static func resolve<T>(type: T.Type = T.self) -> T {
+        let dependencyName = String(describing: T.self)
+        guard let resolver = registrations[dependencyName] else {
+            fatalError("No registration for type \(dependencyName)")
+        }
+        guard let result = resolver() as? T else {
+            fatalError("Can't cast registration to type \(dependencyName)")
+        }
+        return result
+    }
+    
+    static func remove<T>(type: T.Type) {
+        let dependencyName = String(reflecting: T.self)
+        registrations.removeValue(forKey: dependencyName)
+    }
+}
